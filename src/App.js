15c10facaf57629702d235dc9db9caabf02a3033
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-07 22:04:14 -0800

    card amd card-list added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import './App.css';
+//for class
+import { Component } from "react";
+import React from "react";
+import { CardList } from './components/card-list/card-list.component';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a>
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* we are passing monsters as prop  */}
+        <CardList monsters = {this.state.monsters} />
+          {/* {this.state.monsters.map(monster => (
+          <h3 key={monster.id}> {monster.name} </h3> */}
+      </div>
+    );
+  }
 }
 
 export default App;
+
